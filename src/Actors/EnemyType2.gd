814434eff5d1fce96331587d84ac610eeec3df86
Oana Dima <oanadima26@gmail.com>
2021-04-11 10:03:17 +0300

    Finish the dragon

--- src/Actors/EnemyType2.gd
+++ src/Actors/EnemyType2.gd
@@ -1,6 +1,7 @@
-#extends KinematicBody2D
+
 extends "res://src/Actors/Actor.gd"
-# abilitatea de a arunca cu bombe
+
+# abilitatea de a arunca cu flacari
 onready var BULLET_SCENE = preload ("res://src/Actors/Bullet.tscn")
 
 var player = null
@@ -9,16 +10,20 @@ var move = Vector2.ZERO
 
 func _ready() -> void:
 	set_physics_process(false)
-	# daca ar fi pzoitiva ar lua-o spre stanga => merge spre jucator
+	# daca ar fi negativa ar porni spre stanga => merge spre jucator
 	# directia normala de mers este spre dreapta de-a lungul axei ox
-	_velocity.x = -speed.x * 1.5 # maresc viteza inamicului	
+	_velocity.x = -speed.x * 1.5
+	
 	
 func _physics_process(delta):
 	_velocity.y += gravity * delta
+	
 	if is_on_wall():
-		_velocity.x *= -1.0
+		_velocity.x *= -1.0 #schimbare directie de deplasare
+		$enemy2.scale.x *= -1.0 #rotatie imagine inamic cand isi schimba directia
+		
 	_velocity.y =  move_and_slide(_velocity, FLOOR_NORMAL).y
-
+	
 	
 func _on_Area2D_body_entered(body): # semnal pt Area2D
 		player = body 
@@ -31,16 +36,20 @@ func fire(): #pentru aruncarea cu bombe
 	bullet.position = get_global_position() # va incepe de la pozitia aruncatorului
 	bullet.player = player # se duce catre player
 	get_parent().add_child(bullet)
-	$Timer.set_wait_time(0.7) #intervalul la care se arunca bombele
+	$Timer.set_wait_time(0.5) #intervalul la care se arunca bombele
 
 func _on_Timer_timeout(): # semnal pt Area2D
 	# rol: pentru a controla cat de rapid inamicul aruna cu bombe
 	if player != null: 
 		fire()
 
-
+# preluata de la inamicul de tipul 1
 func _on_StompDetector_body_entered(body):
+	print("stomp_detec")
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return 
 	get_node("CollisionShape2D").disabled=true
 	queue_free()
+
+
+	
