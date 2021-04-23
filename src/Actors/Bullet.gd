b1aa5477308940e3af65f58692349b14509c7575
Oana Dima <oanadima26@gmail.com>
2021-04-23 23:03:49 +0300

    Complet level 2

--- src/Actors/Bullet.gd
+++ src/Actors/Bullet.gd
@@ -1,18 +1,21 @@
 extends KinematicBody2D
 
 var move = Vector2.ZERO
-var look_vec = Vector2.ZERO # aici o sa avem directia playerului
+# va retine directia playerului
+var player_direction = Vector2.ZERO 
 var player = null
-export var speed = 3
-
+#viteza de deplasare a bombelor
+export var speed = 3 
 
 func _ready():
-	look_vec = player.position - global_position #pozitia playerului - pozitia inamicului 
-	look_vec.y -= 50 # pentru a fi mai orientate catre player
+	#pozitia playerului - pozitia inamicului 
+	player_direction = player.position - global_position 
+	# pentru a fi mai orientate catre player
+	player_direction.y -= 50 
 func _physics_process(delta):	
 	move = Vector2.ZERO
 	
-	move = move.move_toward(look_vec, delta) 
+	move = move.move_toward(player_direction, delta) 
 	move = move.normalized() * speed
 	position += move
 
