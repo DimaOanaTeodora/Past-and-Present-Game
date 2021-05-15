eb6083471fa684dfd130b2bd9fcb90963b92d95e
veronica242001 <coiculescuveronica@gmail.com>
2021-05-15 17:29:51 +0300

    level 70's

---
+++ Level 70's/src/Actors/Enemy.gd
@@ -0,0 +1,22 @@
+extends "res://src/Actors/Actor.gd"
+
+func _ready() -> void:
+	set_physics_process(false) # dezacTiveaza inimamicul la inceput
+	_velocity.x = -speed.x
+
+func _on_StompDetector_body_entered(body: PhysicsBody2D) -> void:
+		#if body.global_position.y > get_node("StompDetector").global_position.y:
+				#return
+		#get_node("CollisionShape2D").disabled = true
+		queue_free() # will delete the enemy
+		
+
+
+
+func _physics_process(delta: float) -> void:
+	_velocity.y  += gravity * delta
+	if is_on_wall():  # se va updata doar daca mutam inamaicul
+		_velocity.x *= -1.0
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
+
+
