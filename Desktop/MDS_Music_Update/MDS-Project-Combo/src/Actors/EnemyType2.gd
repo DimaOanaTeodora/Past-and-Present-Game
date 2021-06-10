d88609f9c9b1e1d809cdd36323d3e0fb2385fdbd
daria68 <dariadirstariu493@gmail.com>
2021-06-10 22:36:05 +0300

    Music Update

---
+++ Desktop/MDS_Music_Update/MDS-Project-Combo/src/Actors/EnemyType2.gd
@@ -0,0 +1,50 @@
+
+extends "res://src/Actors/Actor.gd"
+
+# abilitatea de a arunca cu flacari
+onready var BULLET_SCENE = preload ("res://src/Actors/BulletMed.tscn")
+
+var player = null
+var move = Vector2.ZERO
+
+
+func _ready() -> void:
+	set_physics_process(false)
+	# daca ar fi negativa ar porni spre stanga => merge spre jucator
+	# directia normala de mers este spre dreapta de-a lungul axei ox
+	if speed.x <0 :
+		$enemy2.scale.x *= -1.0
+	_velocity.x = -speed.x * 1.5
+	
+	
+func _physics_process(delta):
+	_velocity.y += gravity * delta
+	
+	if is_on_wall():
+		_velocity.x *= -1.0 #schimbare directie de deplasare
+		$enemy2.scale.x *= -1.0 #rotatie imagine inamic cand isi schimba directia
+		
+	_velocity.y =  move_and_slide(_velocity, FLOOR_NORMAL).y
+	
+	
+func _on_Area2D_body_entered(body): # semnal pt Area2D
+		player = body 
+
+func _on_Area2D_body_exited(body): # semnal pt Area2D
+	player = null # nu vrem sa mai fie urmarit
+
+func fire(): #pentru aruncarea cu bombe
+	var bullet = BULLET_SCENE.instance() #intanstiere scena
+	bullet.position = get_global_position() # va incepe de la pozitia aruncatorului
+	bullet.player = player # se duce catre player
+	get_parent().add_child(bullet)
+	$Timer.set_wait_time(1.2) #intervalul la care se arunca bombele
+
+func _on_Timer_timeout(): # semnal pt Area2D
+	# rol: pentru a controla cat de rapid inamicul aruna cu bombe
+	if player != null: 
+		fire()
+
+
+
+	
---
+++ Desktop/MDS_Music_Update/MDS-Project-Combo/src/Actors/EnemyWithBullet.gd
@@ -0,0 +1,37 @@
+extends KinematicBody2D
+
+onready var BULLET_SCENE = preload("res://src/Actors/Bullet80s.tscn")
+
+var player = null
+var move = Vector2.ZERO
+var speed = 10
+
+func _physics_process(delta):
+	move = Vector2.ZERO
+	
+	if player != null:
+		move = position.direction_to(player.position) * speed
+	else:
+		move = Vector2.ZERO
+		
+	move = move.normalized()
+	move = move_and_collide(move)
+	
+func _on_Area2D_body_entered(body):
+	if body != self:
+		player = body
+
+
+func _on_Area2D_body_exited(body):
+	player = null
+
+func fire():
+	var bullet = BULLET_SCENE.instance()
+	bullet.position = get_global_position()
+	bullet.player = player
+	get_parent().add_child(bullet)
+	$Timer.set_wait_time(1)
+
+func _on_Timer_timeout():
+	if player != null:
+		fire()
